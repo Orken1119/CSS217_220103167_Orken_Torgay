62a5a2680bfb51d15bb238871df74cc9a652432d
Orken1119 <ork.en389@gmail.com>
2026-09-22 10:02:43 +0500

    stage 2

--- week 4/ThermostatAdapter.java
+++ week 4/ThermostatAdapter.java
@@ -3,26 +3,46 @@ public class ThermostatAdapter implements SmartDevice {
 
     public ThermostatAdapter(LegacyThermostat thermostat) {
         if (thermostat == null) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Thermostat cannot be null");
         }
         this.thermostat = thermostat;
     }
 
     @Override
     public void turnOn() {
+        if ("IDLE".equals(thermostat.checkDial())) {
+            thermostat.rotateDial("LOW");
+        }
     }
 
     @Override
     public void turnOff() {
+        thermostat.rotateDial("IDLE");
     }
 
     @Override
     public boolean isOn() {
-        return false;
+        String state = thermostat.checkDial();
+        return "LOW".equals(state) || "MEDIUM".equals(state) || "MAX".equals(state);
     }
 
     @Override
     public int getPowerPercent() {
-        return 0;
+        String state = thermostat.checkDial();
+        if (state == null) {
+            return -1;
+        }
+        switch (state) {
+            case "IDLE":
+                return 0;
+            case "LOW":
+                return 33;
+            case "MEDIUM":
+                return 66;
+            case "MAX":
+                return 100;
+            default:
+                return -1;
+        }
     }
 }
